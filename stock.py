97da31a4b5bd7f0a5769ce55cf38ba346ba7364c
58254451@qq.com <58254451@qq.com>
2014-10-06 17:53:57 +0800

    2014-10-6 更新计算相关性

--- stock.py
+++ stock.py
@@ -5,7 +5,7 @@ import csv
 import pymysql
 import time
 
-def timer (stockid):#核心函数，查URL写数据库
+def timer (stockid):#核心函数，查URL写数据库,计算指标库
 	testhtml=urllib.request.urlopen("http://table.finance.yahoo.com/table.csv?s="+stockid).read()
 	data_line=testhtml.decode("utf8").split('\n')
 	del data_line[0]
@@ -16,15 +16,31 @@ def timer (stockid):#核心函数，查URL写数据库
 		sql="insert into stock.stock values('"+stockid+"','"+datas[0]+"','"+datas[2]+"','"+datas[3]+"','"+datas[1]+"','"+datas[4]+"','"+datas[6]+"','"+datas[5]+"',null,null)"
 		#print (sql)
 		cur.execute(sql)
-	print(stockid+" is ok") 
+	print(stockid+" is ok"+str(time.time()-time1)) 
+
 	conn.commit()
 
+def target():#写入各种参照值
 
-def calc(stockid):#计算大盘赶超率和大盘相关度
-	sql="INSERT INTO clac1(stockid,relativity,over) SELECT a.stockid,AVG(ABS(c-d)),AVG(c-d) FROM (SELECT a.stockid,a.date,(a.close - b.close)/b.close AS c FROM (SELECT  (@i:=@i+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT @i:=1) AS it WHERE stockid='"+stockid+"' ORDER BY DATE DESC) AS a ,(SELECT  (@j:=@j+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT   @j:=0) AS it WHERE stockid='"+stockid+"' ORDER BY DATE DESC) AS b WHERE a.i=b.i) a ,(SELECT a.stockid,a.date, (a.close - b.close)/a.close AS d FROM (SELECT  (@k:=@k+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT   @k:=1) AS it WHERE stockid='000300.ss' ORDER BY DATE DESC) AS a ,(SELECT  (@l:=@l+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT   @l:=0) AS it WHERE stockid='000300.ss'  ORDER BY DATE DESC) AS b WHERE a.i=b.i ) b WHERE a.date=b.date "
+	sql="INSERT INTO target SELECT a.date,a.close, (a.close - b.close)/a.close  FROM (SELECT  (@i:=@i+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT   @i:=1) AS it WHERE stockid='000300.ss' ORDER BY DATE DESC) AS a ,(SELECT  (@j:=@j+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT   @j:=0) AS it WHERE stockid='000300.ss' ORDER BY DATE DESC) AS b WHERE a.i=b.i"
 
 	cur.execute(sql)
-	print(stockid+" 计算 is ok") 
+	print("000300.ss target is ok")
+	conn.commit()
+def calc(stockid):#计算个股与指标之间的相关度
+	pearson_1=[]
+	pearson_2=[]
+	sql=" SELECT a.c,b.`000300.ss_clac` FROM (SELECT a.date,a.close, (a.close - b.close)/a.close AS c FROM (SELECT  (@i:=@i+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT   @i:=1) AS it WHERE stockid='"+stockid+"' ORDER BY DATE DESC) AS a ,(SELECT  (@j:=@j+1)   AS   i,stockid,DATE,CLOSE FROM stock ,(SELECT   @j:=0) AS it WHERE stockid='"+stockid+"' ORDER BY DATE DESC) AS b WHERE a.i=b.i ) a,target b WHERE a.date =b.date "
+
+	cur.execute(sql)
+	r_clac=cur.fetchall()
+	for r in r_clac:
+		pearson_1.append(float(r[0]))
+		pearson_2.append(float(r[1]))
+	#print(str(pearson(pearson_1,pearson_2)))
+	sql="insert into clac1 (stockid, relativity) values ('"+stockid+"','"+str(pearson(pearson_1,pearson_2))+"')"
+	cur.execute(sql)
+	print(stockid+" 计算 is ok 总计用时间"+str(time.time()-time1)) 
 	conn.commit()
 
 def pearson(x,y):
@@ -45,22 +61,29 @@ def pearson(x,y):
 	r=num/den
 	return r
 if __name__ == "__main__":
+	time1=time.time()
 	threads=[]
 	pearson_1=[]
 	pearson_2=[]
 	conn=pymysql.connect(host='localhost',user='root',passwd='123456',db='stock',port=3306)
 	curall=conn.cursor()
-	#curall.execute("delete from stock")
-	#curall.execute("delete from clac1")
 	cur=conn.cursor()
-	curall.execute("SELECT * from tmp ")
+	#curall.execute("delete from stock")
+	#curall.execute("delete from target")
+	#curall.execute("delete from clac1")
+	curall.execute("SELECT CONCAT(CODE,class) FROM stock_code2 WHERE STATUS=1")
 	stone=curall.fetchall()
 	for r in stone:
-		pearson_1.append(float(r[0]))
-		pearson_2.append(float(r[1]))
-
-		#timer(str(r[0]))
-	print(pearson(pearson_1,pearson_2))
-	#for r in stone:
-	#	calc(str(r[0]))
-	#conn.commit()
+		try:
+			timer(str(r[0]))
+		except:
+			continue
+	#target()
+	for r in stone:
+		try:
+			calc(str(r[0]))
+		except:
+			continue
+	conn.commit()
+	
+	
